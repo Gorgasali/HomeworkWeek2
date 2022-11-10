c7b408c1f785359209cc9768e88d54a431d05b42
Gorgasali <57874197+Gorgasali@users.noreply.github.com>
2022-11-10 00:26:29 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+package com.yearup.week2.homework;
+
+public interface Calculator {
+    double sum(double num1, double num2);
+
+    //Method to calculate area which accepts two arguments of type double returns double
+   double area(double length, double width);
+   double volume(double length, double width, double height);
+
+
+}
